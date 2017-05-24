2e850f13363c2addf35335ec6bf8cdd7e3e54a6c
KedemBarak <Kedem.Barak@gmail.com>
2017-05-24 17:25:19 +0300

    2 function to return Greater/Smaller value between 2 elements
    2 function to return Greater/Smaller value between 2 elements

---
+++ src/main/java/com/mapr/drill/Greater.java
@@ -0,0 +1,39 @@
+package com.mapr.drill;
+
+import org.apache.drill.exec.expr.DrillSimpleFunc;
+import org.apache.drill.exec.expr.annotations.FunctionTemplate;
+import org.apache.drill.exec.expr.annotations.FunctionTemplate.FunctionScope;
+import org.apache.drill.exec.expr.annotations.FunctionTemplate.NullHandling;
+import org.apache.drill.exec.expr.annotations.Output;
+import org.apache.drill.exec.expr.annotations.Param;
+import org.apache.drill.exec.expr.holders.Float8Holder;
+import org.apache.drill.exec.expr.holders.IntHolder;
+
+/**
+ * This is the UDF example from http://drill.apache.org/docs/adding-custom-functions-to-drill/
+ * modified slightly to use floating point inputs.
+ *
+ * The idea here is that thus function can be called from a query like this:
+ *
+ * select myaddints(position_id, 3) from cp.`employee.json` limit 3;
+ */
+public class Greater {
+    @FunctionTemplate(name = "drillGreater", scope = FunctionScope.SIMPLE, nulls = NullHandling.NULL_IF_NULL)
+    public static class IntIntGreater implements DrillSimpleFunc {
+        @Param
+        Float8Holder in1;
+        @Param
+        Float8Holder in2;
+        @Output
+        Float8Holder out;
+
+        public void setup() {
+        }
+
+        public void eval() {
+			if(in1.value > in2.value){
+				 out.value = in1.value;
+        } else {
+			out.value = in2.value;
+    }
+}
